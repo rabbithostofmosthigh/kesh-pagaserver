7bac3f491266c427492dca751f3a258b0bd3bec2
rabbithostofmosthigh <rabbithostofmosthigh@gmail.com>
2026-08-16 03:27:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "versanaijarrah9@gmail.com";
-const pass = "jecmnnjioauanuor";
+//const pass = "jecmnnjioauanuor";
 // 2nd
 
 // Middleware
